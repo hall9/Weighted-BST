add7ca6f5dc78c33213dd35aa0d4014fed94d8f6
Alexander Hall: Work Mac <Alexander.d.hall@gmail.com>
2012-03-28 15:42:15 -0400

    Added test file and added the main with reading file funtion

--- src/proj2/Proj2.java
+++ src/proj2/Proj2.java
@@ -1,1 +1,16 @@
-// Alex Hall
+
+package proj2;
+
+import proj2.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Proj2 {
+
+	public static void main(String[] args) throw IOException {
+		
+	}
+}
